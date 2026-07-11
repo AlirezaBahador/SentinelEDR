9840f4fec75afb416e5885517cfa1c3d29a4a39b
Alireza Bahador <168124787+AlirezaBahador@users.noreply.github.com>
2026-07-11 11:35:05 +0330

    Create main.py

---
+++ agent/main.py
@@ -0,0 +1,29 @@
+import psutil
+import time
+from datetime import datetime
+
+known_pids = set()
+
+print("[+] SentinelEDR Agent Started")
+
+while True:
+    for proc in psutil.process_iter(['pid', 'name', 'username', 'cmdline']):
+        try:
+            pid = proc.info['pid']
+
+            if pid not in known_pids:
+                known_pids.add(pid)
+
+                print(f"""
+[{datetime.now()}]
+PID: {proc.info['pid']}
+Process: {proc.info['name']}
+User: {proc.info['username']}
+Command: {' '.join(proc.info['cmdline'])}
+                """)
+
+        except (psutil.NoSuchProcess,
+                psutil.AccessDenied):
+            pass
+
+    time.sleep(1)
